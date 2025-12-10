05832a4dd106188156431422a9049933c11aad76
Aaron Rosas <46721112+Aaronrss@users.noreply.github.com>
2025-12-09 20:25:16 -0600

    AARR-000: Test

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://aaronrss.github.io',
+  output: 'static',
+  integrations: [mdx(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      host: '0.0.0.0',
+      port: 5000,
+      allowedHosts: true
+    }
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  }
+});
